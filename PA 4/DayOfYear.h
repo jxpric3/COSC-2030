e5f2d6df627795607d910dbc2df08564c97caf80
Joshua Price <price.joshua.robert@gmail.com>
2026-09-13 13:00:55 -0600

    Auto-backup 2026-09-13 13:00:55 MDT

---
+++ PA 4/DayOfYear.h
@@ -0,0 +1,16 @@
+//DayOfYear.h 
+#ifndef DayOfYear.h 
+#define DayOfYear.h
+
+using namespace std;
+
+class DayOfYear
+{
+    private: 
+        int dayNumber; 
+        static const string monthNames[12]; 
+
+    public: 
+        void print(int dayInput); 
+
+}
