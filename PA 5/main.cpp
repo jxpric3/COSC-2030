53c574a8f745f6a4911674f45c2fd56c81eb9a65
Joshua Price <price.joshua.robert@gmail.com>
2026-09-19 15:00:55 -0600

    Auto-backup 2026-09-19 15:00:55 MDT

--- PA 5/main.cpp
+++ PA 5/main.cpp
@@ -8,24 +8,42 @@
 
 using namespace std;
 
+void testHelpFunction(const PoliceOfficer &officer, int minutesParked, int minutesPurchased, const string &label);
+
 int main()
 {
+    cout << "TEST 1: Purchased 120, Parked 60" << endl;
+    cout << "----------------------------------------" << endl;
+    
     ParkedCar carOne("Toyota", "Camry", "blue", "099YDS", 123); 
     PoliceOfficer officerOne("Officer Baker", 998885456); 
-    ParkingMeter parkingMeterOne;
+    ParkingMeter parkingMeterOne(45);
 
     if(officerOne.isTimeExpired(carOne, parkingMeterOne))
     {
         ParkingTicket ticketOne = officerOne.createTicket(carOne, parkingMeterOne.getMinutesPurchased());
         cout << "Full Ticket Report: " << endl; 
         cout << "Officer Name: " << ticketOne.getOfficerName() << endl;
-        cout << "Officer Name: " << ticketOne.getOfficerBadgeNumber() << endl;
-        cout << "Parking Fine Amount: " << ticketOne.getFineAmount() << endl; 
-        cout << "Ticketed Car Information: "
+        cout << "Officer Badge Number: " << ticketOne.getOfficerBadgeNumber() << endl;
+        cout << "Parking Fine Amount: " << "$" << ticketOne.getFineAmount() << endl; 
+        cout << "Ticketed Car Information: " << endl; 
+        cout << "Car License Number: " << ticketOne.getCarLicense() << endl; 
+        cout << "Car Make: " << ticketOne.getCarMake() << endl; 
+        cout << "Car Model: " << ticketOne.getCarModel() << endl; 
+        cout << "Car Color: " << ticketOne.getCarColor() << endl; 
+        cout << endl;
+        
 
     }
+    else{
+        cout << "The vehicle is still within its purchased time limit. Do not issue a ticket.";
+        cout << endl;
+    }
 
-  
-    return 0; 
+    
+}
 
+void testHelpFunction(const PoliceOfficer &officer, int minutesParked, int minutesPurchased, const string &label)
+{
+    
 }
