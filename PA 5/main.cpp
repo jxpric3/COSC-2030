f307e12508e317d957f0de38f5d3eb8d03781e2e
Joshua Price <price.joshua.robert@gmail.com>
2026-09-19 13:02:55 -0600

    Auto-backup 2026-09-19 13:02:55 MDT

---
+++ PA 5/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string> 
+
+#include "ParkedCar.h"
+#include "ParkingMeter.h"
+#include "PoliceOfficer.h"
+#include "ParkingTicket.h"
+
+int main()
+{
+    ParkedCar carOne("Toyota", "Camry", "blue", "099YDS", 123); 
+    PoliceOfficer officerOne("Officer Baker", 998885456); 
+    ParkingMeter parkingMeterOne();
+
+    
+
+
+    return 0; 
+
+}
